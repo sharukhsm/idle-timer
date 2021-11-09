80c894ffd0ebba549f9d7d6c4b31a3bf7a9a5957
Sharukh <sharukhsm@gmail.com>
2021-11-09 16:09:56 +0530

    basic version timer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import IdleTimerContainer from "./components/IdleTimerContainer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <IdleTimerContainer></IdleTimerContainer>
+    </div>
+  );
 }
 
 export default App;
--- src/components/IdleTimerContainer.js
+++ src/components/IdleTimerContainer.js
@@ -1,8 +1,21 @@
+import React, { useRef } from "react";
 import IdleTimer from "react-idle-timer";
-import React from "react";
 
 function IdleTimerContainer() {
-  return <div></div>;
+  const IdleTimerRef = useRef(null);
+  const onIdle = () => {
+    console.log("You are idle for 5 seconds");
+  };
+
+  return (
+    <div>
+      <IdleTimer
+        ref={IdleTimerRef}
+        timeout={5 * 1000}
+        onIdle={onIdle}
+      ></IdleTimer>
+    </div>
+  );
 }
 
 export default IdleTimerContainer;
